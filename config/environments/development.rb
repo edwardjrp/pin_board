7605c7366da417fa73d82515cbce0f69fd3af175
Edward Rodriguez <edwardjrp@gmail.com>
2015-05-21 18:10:58 -0400

    Add image uploading with paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,7 @@ Rails.application.configure do
   #Needed for devise gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #Needed for paperclip because it uses ImageMagick
+  Paperclip.options[:command_path] = '/usr/local/bin/'
+
 end
